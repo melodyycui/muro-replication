3f439f2d8460ea0eab76e72d93213a7aa48a5e5e
Melody Cui <mc3803@princeton.edu>
2025-06-10 10:58:57 -0400

    nothing

--- packsimulation.jl
+++ packsimulation.jl
@@ -1,4 +1,5 @@
-using Agents, LinearAlgebra, Random
+using Agents
+using LinearAlgebra
 
 
 #= NEED TO CONSIDER
@@ -7,10 +8,9 @@ using Agents, LinearAlgebra, Random
 3. need to calibrate magnitudes of things, such as size of grid, velocity (just in case vel too big, go out of bounds)
 =#
 
-# Maybe add health here too?
+# Maybe add health here too
 @agent struct Wolf(ContinuousAgent{2, Float64})
 #    group::Int
-    vel::Vector{Float64}
 end
 
 @agent struct Sheep(ContinuousAgent{2, Float64})
@@ -23,32 +23,19 @@ ww_force_coefficient = 0.5
 sw_force_coefficient = 2 # force of sheep on wolf
 dt = 1
 
-# not sure if this function is relevant or needed
-function add_agent_single(model)
-    pos = rand(model.space)
-    vel = zeros(2)
-    return add_agent!(Wolf(pos, vel), model)
-end
-
 function wolf_step!(predator, prey, model)
 
     current_distance = norm(prey.pos - predator.pos)
     wolf_encounter_speed = 10 # this is an arbitrary speed choice
 
     # for each neighbor wolf, find repulsive force α distance
-    wolf_repulsion = zeros(2)
+    wolf_repulsion = [0, 0]
     for neighbor in nearby_agents(predator, model)
-        
-        if neighbor.id == predator.id
-            continue
-
+            
         # sum up the repulsive force vectors to get acceleration
-        else
-            e = 1e-8   
-            distance_between = max(norm(predator.pos - neighbor.pos), e)
+        distance_between = norm(predator.pos - neighbor.pos)
+        wolf_repulsion += ww_force_coefficient * (predator.pos - neighbor.pos) / (distance_between)^2
 
-            wolf_repulsion += ww_force_coefficient * (predator.pos - neighbor.pos) / (distance_between)^2
-        end
     end
     
     if (current_distance <= min_safe_distance)
@@ -63,7 +50,7 @@ function wolf_step!(predator, prey, model)
 
         predator.vel = proj_u_v
 
-    else
+    else # (wolf is outside critical distance)
 
         # impact of sheep attraction on distance to sheep
         wolf_encounter_velocity = (prey.pos - predator.pos) / norm(prey.pos - predator.pos) * wolf_encounter_speed
@@ -79,7 +66,6 @@ end
 
 using Random: Xoshiro # access the RNG object
 
-# predator.pos = clamp.(predator.pos, 0, model.space.extent)
 function initialize(; total_agents = 5, size = (10.0, 10.0), min_safe_distance = 0.1, seed = 125)
     space = ContinuousSpace(size; periodic = false;)
     properties = Dict(:min_safe_distance => min_safe_distance)
