0cc34390248f42243a9687e14211321a6a2ef1ef
Melody Cui <mc3803@princeton.edu>
2025-06-12 14:36:10 -0400

    trying out abmstepper

--- packsimulation.jl
+++ packsimulation.jl
@@ -99,5 +99,9 @@ ac(a::Animal) = a.group == 1 ? :blue : :green
 as(a::Animal) = a.group == 1 ? 13 : 10
 am = 'o'
 
-abmvideo("wolf_hunt.mp4", model;
-title = "Wolf Hunt Simulation", framerate = 15, frames = 20, ac, as, am)
+fig, abmstepper = InteractiveDynamics.abm_plot(model; ac, as, am)
+step!(abmstepper, model, agent_step! = animal_step!, model_step!, 5)
+fig
+
+# abmvideo("wolf_hunt.mp4", model;
+# title = "Wolf Hunt Simulation", framerate = 15, frames = 20, ac, as, am)
