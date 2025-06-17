935256bffb7acf8087364aed70c333403933ae51
Hellen Ding <helleno@dynamic-oit-ip4-wifirestricted03-10-49-32-114.princeton.edu>
2025-06-17 12:59:08 -0400

    Added comments to fmoving_hunt_sim and changed wolf_encounter_speed name to wolf_chase_speed to match what we did with fstation_hunt_sim

--- function_code/fmoving_hunt_sim.jl
+++ function_code/fmoving_hunt_sim.jl
@@ -1,38 +1,45 @@
 using Pkg, InteractiveDynamics, CairoMakie, Agents, LinearAlgebra
 using Random: Xoshiro
 
+# Creating Wolf agent type
 @agent struct Wolf(ContinuousAgent{2, Float64})
 end
 
+# Creating Sheep agent type
 @agent struct Sheep(ContinuousAgent{2, Float64})
 end
 
-# Function to move a sheep at each time step
+# Function to move a sheep at each time step, assuming it moves in a circular path with decreasing velocity
 function animal_step!(agent::Sheep, model)
 
-    center = model.center
-    dt = model.dt
-    sheep_tangent_acceleration = model.sheep_tangent_acceleration
+    center = model.center # center of the sheep's circular path (arbitrarily chosen by user)
+    dt = model.dt # time step increment for our sim
+    sheep_tangent_acceleration = model.sheep_tangent_acceleration # accel in tangent dir to sheep's circular movement
 
-    rotation_matrix = [cos(pi/2) sin(pi/2); -sin(pi/2) cos(pi/2)]
+    # Find unit vector pointing from sheep to center (radial direction)
     radius = norm(agent.pos - center)
     unit_radial_vector = (center - agent.pos) / radius
 
+    # Find the centripetal acceleration needed for the sheep to follow its circular path
     new_speed = norm(agent.vel) + sheep_tangent_acceleration * dt
     centrip_accel = (new_speed^2/radius)* unit_radial_vector
 
     agent.vel += centrip_accel * dt
 
+    # Project the sheep's velocity onto the tangential direction (obtained by rotating the radial 
+    # vector 90° counterclockwise) to maintain circular motion
+    rotation_matrix = [cos(pi/2) sin(pi/2); -sin(pi/2) cos(pi/2)]
     tangential_vector = rotation_matrix * unit_radial_vector
     proj = tangential_vector * dot(agent.vel, tangential_vector)/norm(tangential_vector)
     agent.vel = proj/norm(proj) * new_speed
 
-    move_agent!(agent, model, dt)
+    # update model with new velocity after dt (timestep increment for simulation)
+    move_agent!(agent, model, dt) 
 
 end
 
 
-# Function to move a wolf at each time step
+# Function to move a wolf at each time step, assuming prey moves in a circular path with decreasing velocity
 function animal_step!(agent::Wolf, model)
 
     # identify the sheep in the model
@@ -45,20 +52,22 @@ function animal_step!(agent::Wolf, model)
     end
 
     # Model the behavior of the wolf
+
     current_distance = norm(sheep.pos - agent.pos)
-    # wolf_encounter_speed = 1.0      # this is an arbitrary speed choice
-    wolf_encounter_speed = model.wolf_encounter_speed
-    min_safe_distance = model.min_safe_distance
-    ww_force_coefficient = model.ww_force_coefficient
+
+    wolf_chase_speed = model.wolf_chase_speed # speed at which wolf chases sheep
+    min_safe_distance = model.min_safe_distance # minimum distance at which wolf will orbit sheep
+    ww_force_coefficient = model.ww_force_coefficient # repulsive force coefficient for wolf-wolf interaction
     dt = model.dt
 
-    # for each neighbor wolf, find repulsive force α distance
-    wolf_repulsion = [0, 0]
+    # each neighbor wolf exerts repulsive force on current wolf agent
+    wolf_repulsion = [0, 0] # stores net wolf-wolf repulsive force vector on current wolf
     for neighbor in allagents(model)
 
         if neighbor.id != agent.id && isa(neighbor, Wolf)
             
-            # sum up the repulsive force vectors to get acceleration
+            # sum up wolf-wolf repulsive force vectors
+            # repulsive force is inversely proportional to distance between wolves
             distance_between = norm(agent.pos - neighbor.pos)
             wolf_repulsion += ww_force_coefficient * (agent.pos - neighbor.pos) / (distance_between)^2
 
@@ -66,39 +75,45 @@ function animal_step!(agent::Wolf, model)
 
     end
     
+    # once wolf is within a critical distance to the sheep, wolf will maintain that critical distance
+    # and orbit around sheep
     if (current_distance <= min_safe_distance)
 
+        # find direction of wolf to sheep, rotate 90 degrees to find tangential movement direction
         rotation_matrix = [cos(pi/2) sin(pi/2); -sin(pi/2) cos(pi/2)]
-
-        # projecting the repulsive force vector onto the tangential vector
-        # to determine the direction the wolf travels along the circle
         u = rotation_matrix * (agent.pos - sheep.pos)
-        dot_product = dot(u, wolf_repulsion)
-        proj_u_v = (dot_product / norm(u)^2) * u * wolf_encounter_speed
 
+        # projecting the repulsive force vector onto the tangential vector and multiply by wolf speed
+        # to determine the velocity the wolf travels along the circle
+        dot_product = dot(u, wolf_repulsion)
+        proj_u_v = (dot_product / norm(u)^2) * u * wolf_chase_speed
         agent.vel = proj_u_v
 
+    # if wolf is outside critical distance from sheep, wolf is attracted to sheep
     else
 
-        # impact of sheep attraction on distance to sheep
-        wolf_encounter_velocity = (sheep.pos - agent.pos) / norm(sheep.pos - agent.pos) * wolf_encounter_speed
+        # wolf moves in direction of sheep due to attractive force exerted by sheep on wolf
+        wolf_chase_velocity = (sheep.pos - agent.pos) / norm(sheep.pos - agent.pos) * wolf_chase_speed
 
-        agent.vel = wolf_encounter_velocity + wolf_repulsion ## THINK about how repulsive forces will impact the velocity 
+        # wolf velocity impacted by both attraction to sheep and repulsion to neighboring wolves
+        agent.vel = wolf_chase_velocity + wolf_repulsion
     
     end
     
+    # update model with new velocity after dt (timestep increment for simulation)
     move_agent!(agent, model, dt)
 end
 
+# this fn initializes agent-based model for wolf hunt of a single prey moving in circular path w/ decreasing velocity
 function initialize(total_agents, size, min_safe_distance, ww_force_coefficient, sw_force_coefficient,
-                   wolf_encounter_speed, dt, seed,
+                   wolf_chase_speed, dt, seed,
                    center, sheep_tangent_acceleration, sheep_init_speed)
     space = ContinuousSpace(size; periodic = false)
     properties = Dict(
         :min_safe_distance => min_safe_distance,
         :ww_force_coefficient => ww_force_coefficient,
         :sw_force_coefficient => sw_force_coefficient,
-        :wolf_encounter_speed => wolf_encounter_speed,
+        :wolf_chase_speed => wolf_chase_speed,
         :dt => dt,
         :center => center,
         :sheep_tangent_acceleration => sheep_tangent_acceleration
@@ -111,29 +126,30 @@ function initialize(total_agents, size, min_safe_distance, ww_force_coefficient,
     )
 
     rotation_matrix = [cos(pi/2) sin(pi/2); -sin(pi/2) cos(pi/2)]
-
-    for n in 1:(total_agents - 1)
-        add_agent!(Wolf, model; vel = (0.0, 0.0))
-    end 
-
     rand_pos = [5 + 10 *rand(rng), 5 + 10*rand(rng)]
     radial_vector = rand_pos - center
     tangential_vector = rotation_matrix * radial_vector
     initial_vel = (tangential_vector/norm(tangential_vector)) * sheep_init_speed
 
+    # adding wolf agents at random positions with velocity = 0
+    for n in 1:(total_agents - 1)
+        add_agent!(Wolf, model; vel = (0.0, 0.0))
+    end 
+
+    # adding a single sheep/prey agent at random position with velocity = 0
     add_agent!(Sheep, model; pos = rand_pos, vel = initial_vel) # add one sheep
     return model
 end
 
-# attempt at a callable function to run the simulation
+# this fn returns a video sim of a wolf pack hunting a single prey moving in circular path w/ decreasing velocity
 function fmoving_hunt_sim(min_safe_distance=1.0, ww_force_coefficient=0.5, sw_force_coefficient=2.0, 
-                          wolf_encounter_speed=1.0, dt=0.25, 
+                          wolf_chase_speed=1.0, dt=0.25, 
                           total_agents=6, size=(20.0, 20.0), seed=125, framerate=15, frames=200,
                           center=[10.0, 10.0], sheep_tangent_acceleration=-0.1, sheep_init_speed=0.5)
 
     model = initialize(
         total_agents, size, min_safe_distance, ww_force_coefficient, sw_force_coefficient,
-        wolf_encounter_speed, dt, seed,
+        wolf_chase_speed, dt, seed,
         center, sheep_tangent_acceleration, sheep_init_speed
     )
 
@@ -155,7 +171,7 @@ fmoving_hunt_sim(
     1.0,                    # min_safe_distance
     0.5,                    # ww_force_coefficient
     2.0,                    # sw_force_coefficient
-    1.0,                    # wolf_encounter_speed (hardcoded inside animal_step! in 2nd version)
+    1.0,                    # wolf_chase_speed (hardcoded inside animal_step! in 2nd version)
     0.25,                   # dt
     6,                      # total_agents (5 wolves + 1 sheep)
     (20.0, 20.0),           # size of the space
