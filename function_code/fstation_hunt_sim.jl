1e0c6064d36c8c488a7bf984232d13309a56c79f
Melody Cui <mc3803@princeton.edu>
2025-06-17 12:04:10 -0400

    commenting

--- function_code/fstation_hunt_sim.jl
+++ function_code/fstation_hunt_sim.jl
@@ -20,52 +20,55 @@ function animal_step!(wolf, model)
 
     # Model the behavior of the wolf
     if wolf.group == 2              # check agent is a wolf
+        
         current_distance = norm(sheep.pos - wolf.pos)
-        # wolf_encounter_speed = 1.0      # this is an arbitrary speed choice
 
-        min_safe_distance = model.min_safe_distance
-        ww_force_coefficient = model.ww_force_coefficient
-        dt = model.dt
-
-        # for each neighbor wolf, find repulsive force α distance
-        wolf_repulsion = [0, 0]
+        # each neighbor wolf exerts repulsive force on current wolf agent
+        wolf_repulsion = [0, 0] # stores net wolf-wolf repulsive force vector on current wolf
         for neighbor in allagents(model)
 
             if neighbor.id != wolf.id && neighbor.group == 2
             
-                # sum up the repulsive force vectors to get acceleration
+                # sum up wolf-wolf repulsive force vectors
+                # repulsive force is inversely proportional to distance between wolves
                 distance_between = norm(wolf.pos - neighbor.pos)
-                wolf_repulsion += ww_force_coefficient * (wolf.pos - neighbor.pos) / (distance_between)^2
+                wolf_repulsion += model.ww_force_coefficient * (wolf.pos - neighbor.pos) / (distance_between)^2
 
             end
 
         end
     
-        if (current_distance <= min_safe_distance)
+        # once wolf is within a critical distance to the sheep, wolf will maintain that critical distance
+        # and orbit around sheep
+        if (current_distance <= model.min_safe_distance)
 
+            # find direction of wolf to sheep, rotate 90 degrees to find tangential movement direction
             rotation_matrix = [cos(pi/2) sin(pi/2); -sin(pi/2) cos(pi/2)]
-
-            # projecting the repulsive force vector onto the tangential vector
-            # to determine the direction the wolf travels along the circle
             u = rotation_matrix * (wolf.pos - sheep.pos)
+
+            # projecting the repulsive force vector onto the tangential vector and multiply by wolf speed
+            # to determine the velocity the wolf travels along the circle
             dot_product = dot(u, wolf_repulsion)
             proj_u_v = (dot_product / norm(u)^2) * u * model.wolf_encounter_speed
-
             wolf.vel = proj_u_v
 
+        # if wolf is outside critical distance from sheep, wolf is attracted to sheep
         else
 
-            # impact of sheep attraction on distance to sheep
-            wolf_encounter_velocity = (sheep.pos - wolf.pos) / norm(sheep.pos - wolf.pos) * model.wolf_encounter_speed
-
-            wolf.vel = wolf_encounter_velocity + wolf_repulsion ## THINK about how repulsive forces will impact the velocity 
+            # wolf moves in direction of sheep due to attractive force exerted by sheep on wolf
+            wolf_chase_velocity = (sheep.pos - wolf.pos) / norm(sheep.pos - wolf.pos) * model.wolf_encounter_speed
+            
+            # wolf velocity impacted by both attraction to sheep and repulsion to neighboring wolves
+            wolf.vel = wolf_chase_velocity + wolf_repulsion
     
         end
     
-        move_agent!(wolf, model, dt)
+        # update model with new velocity after dt (timestep increment for simulation)
+        move_agent!(wolf, model, model.dt)
     end
 end
 
+# this fn initializes our agent-based model for wolf hunt of single stationary prey
 function initialize(total_agents, size, min_safe_distance, ww_force_coefficient, sw_force_coefficient, wolf_encounter_speed, dt, seed)
     space = ContinuousSpace(size; periodic = false)
     properties = Dict(:min_safe_distance => min_safe_distance, :ww_force_coefficient => ww_force_coefficient,
@@ -79,15 +82,17 @@ function initialize(total_agents, size, min_safe_distance, ww_force_coefficient,
         scheduler = Schedulers.Randomly() # all agents are activated once at random
     )
 
+    # adding wolf agents at random positions with velocity = 0
     for n in 1:(total_agents - 1)
         add_agent!(model; group = 2, vel = (0.0, 0.0))
     end 
 
-    add_agent!(model; group = 1, vel = (0.0, 0.0)) # add one sheep
+    # adding a single sheep/prey agent at random position with velocity = 0
+    add_agent!(model; group = 1, vel = (0.0, 0.0))
     return model
 end
 
-# attempt at a callable function to run the simulation
+# this fn returns a video simulation of a wolf pack hunting a single stationary prey
 function fstation_hunt_sim(min_safe_distance=2.0, ww_force_coefficient=0.5, sw_force_coefficient=2, 
                         wolf_encounter_speed=2.0, dt=0.05, 
                         total_agents=6, size=(20.0, 20.0), seed=125, framerate=15, frames=300)
